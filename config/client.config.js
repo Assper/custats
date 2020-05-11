57b8390b8794ca2c77fd766333b0a073fabc53f0
Assper <theassper@gmail.com>
2020-05-11 18:25:52 +0300

    Add analyze scripts

--- config/client.config.js
+++ config/client.config.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const public = path.resolve(__dirname, '../public')
 const entry = path.resolve(__dirname, '../src/client/index.jsx')
 const template = path.resolve(__dirname, '../src/client/index.html')
+
+const analyze = process.env.ANALYZE === 'true'
+const plugins = [new HtmlWebpackPlugin({ template })]
+if (analyze) plugins.push(new BundleAnalyzerPlugin())
 
 const client = {
   entry,
@@ -25,9 +30,7 @@ const client = {
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    template
-  })]
+  plugins
 }
 
 module.exports = client
